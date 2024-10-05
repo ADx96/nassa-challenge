cea6bb1bffd2fded8cea5efbb2c1c7228ac278c2
Abduljawad AbuAlrous <abduljawad.abualrous@kw.zain.com>
2024-10-05 11:01:41 +0300

    feat: added new

--- src/api/users.js
+++ src/api/users.js
@@ -1,8 +1,7 @@
-import axiosInstance from './axios'; // Import the Axios instance
+import axiosInstance from './axios';
 
 const fetchUsers = async () => {
-  const response = await axiosInstance.get('/posts'); // Use the Axios instance to make the request
-  return response.data; // Return the data from the response
+  const { data } = await axiosInstance.get('/posts');
+  return data;
 };
-
 export { fetchUsers };
